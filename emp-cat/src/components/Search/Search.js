60bbd16cff67fb4e95b5ead73f01a320b0beafa7
Tajah Johnson <tajah93@hotmail.com>
2020-11-19 22:20:52 -0600

    Edited form syntax

--- emp-cat/src/components/Search/Search.js
+++ emp-cat/src/components/Search/Search.js
@@ -8,14 +8,14 @@ const Search = () => {
     const searchContext = useContext(EmpContext);
 
     return (
-        <div className="topnav">
-            <form className="submitForm">
-                <input type="search" placeholder="Search by Name" aria-label="Search"
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search by Name" aria-label="Search"
                     onChange={e => EmpContext.handleSearchChange(e)}
                 />
-                <button className="Submit" value="submit">Search</button>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
-        </div>
+        </nav>
 
 
     )

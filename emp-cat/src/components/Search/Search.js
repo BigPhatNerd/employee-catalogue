e4b07297a8627b327725dde6e4d60f0f1e625e88
Tajah Johnson <tajah93@hotmail.com>
2020-11-20 20:31:55 -0600

    Removed useContext

--- emp-cat/src/components/Search/Search.js
+++ emp-cat/src/components/Search/Search.js
@@ -1,24 +1,25 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./Search.css";
-import EmpContext from "../../utils/EmpContext";
+// import EmpContext from "../../utils/EmpContext";
 
 
 
-const Search = () => {
-    const searchContext = useContext(EmpContext);
+function Search({handleSearchChange}) {
+    // const searchContext = useContext(EmpContext);
+    // console.log(searchContext)
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <form className="form-inline my-2 my-lg-0">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search by Name" aria-label="Search"
-                    onChange={e => EmpContext.handleSearchChange(e)}
-                />
+                    onChange={event => handleSearchChange(event)}/>
+                 
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </nav>
 
 
     )
-}
+};
 
 export default Search;
